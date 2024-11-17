a51a20b84c6f92da4f22bd6b9d492f1db01e72a9
farisqlail <faris.riskilail@gmail.com>
2024-11-17 21:47:36 +0700

    add: cors PATCH

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -19,7 +19,7 @@ class CorsMiddleware
 
         // Tambahkan header CORS
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
         // Untuk menangani preflight request (OPTIONS)
